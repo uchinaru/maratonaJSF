f6ffbdcf78f3a458149887d5e239161a90e1aff4
Jonatas Benicio <jonathas_santos93@hotmail.com>
2023-05-25 21:29:29 -0300

    Conectando bean ao front e apresentando dados na tela.

--- src/main/java/com/maratonaJSF/controller/GestaoEmpresaBean.java
+++ src/main/java/com/maratonaJSF/controller/GestaoEmpresaBean.java
@@ -8,20 +8,32 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import com.maratonaJSF.models.Empresa;
+import com.maratonaJSF.models.TipoEmpresa;
+
 @Named
 // @RequestScoped // inicia por requisão post, ao acessar ou enviar requições tais como click de buttons.
 @ViewScoped //A sessão se inicia ao usuario carregar ou acessar, e finaliza ao fechar ou outcome por qualquer componente com tal função.
 // @SessionScoped //A Sessão inicia e finaliza com o carregamento no navegador.
 // @ApplicationScoped // Uma unica sessão existente por aplicação rodando.
 public class GestaoEmpresaBean implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
-	private static Integer Numero = 0;
 	
-	public GestaoEmpresaBean() {
-		Numero++;
+	private Empresa empresa = new Empresa();
+	
+	public void salvar() {
+		System.out.println("Razão Social: " + empresa.getRazaoSocial() + 
+				" - Nome fantasia: " + empresa.getNomeFantasia() 
+				+" - Tipo Empresa: " + empresa.getTipo());
 	}
 	
-	public Integer getNumero() {
-		return Numero; 
+	public Empresa getEmpresa() {
+		return empresa;
 	}
+	
+	public TipoEmpresa[] getTiposEmpresas() {
+		return TipoEmpresa.values();
+	}
+	
 }
